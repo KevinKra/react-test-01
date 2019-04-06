c20b8b5df5c58e10205eabfda638941039b6a298
Kevin Krato <kratok08@gmail.com>
2019-04-06 08:46:54 -0600

    Support logIn|Greeting comps

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
 import "../css/App.css";
+import Login from "./Login";
 
 const Title = ({ text }) => <div>{text}</div>;
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      userName: null
+    };
+  }
   render() {
     return (
-      <div className="App">
-        <Title text="Practice Playground" />
-        <p>The intention of this file is to practice various React features</p>
+      <div className="mainSection">
+        <section className="App">
+          <Title text="Practice Playground" />
+          <p>
+            The intention of this file is to practice various React features
+          </p>
+        </section>
+        <section>
+          <Login userName={this.state.userName} />
+        </section>
       </div>
     );
   }
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,18 +1,29 @@
-import React from 'react';
-
+import React from "react";
+import LoginGreeting from "./LoginGreeting";
+import "../css/Login.css";
 class Login extends React.Component {
   constructor() {
-    super()
+    super();
   }
+  //1. Capture userName input and update App state
+  //2. Conditionally render LoginGreeting if there is a userName
   render() {
-    return() {
+    return (
       <form className="logIn">
-        <label htmlFor="username">
-          <input type="text" name="username"/>
-             </label>
-        <label htmlFor="password"><input type="text"/></label>
+        <LoginGreeting />
+        <div className="userInputs">
+          <label htmlFor="username">
+            Username:
+            <input type="text" name="username" />
+          </label>
+          <label htmlFor="password">
+            Password:
+            <input type="text" />
+          </label>
+        </div>
+        <button type="submit">Submit</button>
       </form>
-    }
+    );
   }
 }
 
