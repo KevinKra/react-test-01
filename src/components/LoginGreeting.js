d6721a8dc550b364bf116800009ba15c1987b963
Kevin Krato <kratok08@gmail.com>
2019-04-06 08:47:39 -0600

    Init LoginGreeting comp

---
+++ src/components/LoginGreeting.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Greeting extends React.Component {
+  constructor() {
+    super();
+  }
+  render() {
+    return (
+      <div>
+        <p>Please Log in</p>
+      </div>
+    );
+  }
+}
+
+export default Greeting;
