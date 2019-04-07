715a5b54624fc93df0262a047ad99576674f8fe3
Kevin Krato <kratok08@gmail.com>
2019-04-07 14:27:16 -0600

    Add notes for component

--- src/components/ClockTwo.js
+++ src/components/ClockTwo.js
@@ -14,10 +14,18 @@ class ClockTwo extends React.Component {
   };
 
   componentDidMount() {
+    //this.intervalID is stored on this and does not participate in the data flow.
     this.intervalID = setInterval(this.tick, 1000);
   }
 
   componentWillUnmount() {
+    //We now tear down the interval in this lifecycle method, this frees the resources back up.
+    //that way the next time the clock is appended to the DOM we dont have the previous iteration still running**
+
+    //For a React component, this is where you would clean up any of those long running processes that you set up in componentDidMount.
+    //In the above data fetching example, all we would have to do is clear the interval so that the weather API would no longer get called every 15 seconds.
+
+    //If the Clock component is ever removed from the DOM, React calls the componentWillUnmount() lifecycle method so the timer is stopped.
     clearInterval(this.intervalID);
   }
 
